1323de8f4c655f60a9b36261ce4601a10c681e5a
Negrusa, Dan <dan.negrusa@experian.com>
2021-01-27 15:33:38 +0100

    cambios

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -1,50 +1,45 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import Filter from "./components/Filter"
-import CountriesList from "./components/CountriesList"
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Filter from './components/Filter';
+import CountriesList from './components/CountriesList';
 
 const App = () => {
-
   const [countries, setCountries] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [countriesToShow, setCountriesToShow] = useState([]);
 
-
-
-
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('https://restcountries.eu/rest/v2/all')
-      .then(response => {
-        console.log('promise fulfilled')
-        if (response && response.data) {
-          console.log('Valid data')
-          setCountries(response.data)
-          setIsLoading(false)
-        }
-      })
-  }, [])
-
+    console.log('effect');
+    axios.get('https://restcountries.eu/rest/v2/all').then((response) => {
+      console.log('promise fulfilled');
+      if (response && response.data) {
+        console.log('Valid data');
+        setCountries(response.data);
+        setIsLoading(false);
+      }
+    });
+  }, []);
 
   return (
-    <>{
-      !isLoading
-        ?
+    <>
+      {isLoading ? (
+        'Loading'
+      ) : (
         <div>
-          <Filter countries={countries} setCountries={setCountries} countriesToShow={countriesToShow} setCountriesToShow={setCountriesToShow} />
-          <CountriesList countriesToShow={countriesToShow} setCountriesToShow={setCountriesToShow} />
+          <Filter
+            countries={countries}
+            setCountries={setCountries}
+            countriesToShow={countriesToShow}
+            setCountriesToShow={setCountriesToShow}
+          />
+          <CountriesList
+            countriesToShow={countriesToShow}
+            setCountriesToShow={setCountriesToShow}
+          />
         </div>
-
-        : "Loading"
-    }</>
-  )
-
-
-
-}
-
-
+      )}
+    </>
+  );
+};
 
 export default App;
--- part2/data-for-countries/src/components/CountrieFull.js
+++ part2/data-for-countries/src/components/CountrieFull.js
@@ -1,75 +1,97 @@
-
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 const CountrieFull = ({ countrie }) => {
-    const flag = countrie.flag
-    const flagStyle = {
-        width: "200px",
-    }
+  const flag = countrie.flag;
+  const flagStyle = {
+    width: '200px',
+  };
 
-
-    return (
-        <div>
-            <h1>{countrie.name}</h1>
-            <div>
-                <p>Capital: {countrie.capital} </p>
-                <p>Population: {countrie.population} </p>
-            </div>
-            <h2>Languages</h2>
-            <ul>
-                {countrie.languages.map(language => <li key={language.name}>{language.name}</li>)}
-            </ul>
-            <img style={flagStyle} src={flag} alt="flag"></img>
-            <WeatherData capital={countrie.capital} />
-        </div>
-    )
-
-}
+  return (
+    <div>
+      <h1>{countrie.name}</h1>
+      <div>
+        <p>Capital: {countrie.capital} </p>
+        <p>Population: {countrie.population} </p>
+      </div>
+      <h2>Languages</h2>
+      <ul>
+        {countrie.languages.map((language) => (
+          <li key={language.name}>{language.name}</li>
+        ))}
+      </ul>
+      <img style={flagStyle} src={flag} alt='flag'></img>
+      <WeatherData capital={countrie.capital} />
+    </div>
+  );
+};
 
 const WeatherData = ({ capital }) => {
-    console.log("WeahterData", capital)
+  console.log('WeahterData', capital);
 
-    const [weather, setWeather] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+  const [weather, setWeather] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const token = process.env.REACT_APP_API_KEY;
+  const token = process.env.REACT_APP_API_KEY;
 
+  useEffect(() => {
+    console.log('effect in WeatherData');
+    axios
+      .get(
+        `http://api.weatherstack.com/current?access_key=${token}&query=${capital}`
+      )
+      .then((response) => {
+        console.log('promise fulfilled in WeatherData');
+        if (response && response.data) {
+          console.log('Valid data in WeatherData');
+          console.log(response.data);
+          setWeather(response.data);
+          setIsLoading(false);
+        }
+      });
+  }, []);
 
-    useEffect(() => {
-        console.log('effect in WeatherData')
-        axios
-            .get(`http://api.weatherstack.com/current?access_key=${token}&query=${capital}`)
-            .then(response => {
-                console.log('promise fulfilled in WeatherData')
-                if (response && response.data) {
-                    console.log('Valid data in WeatherData')
-                    console.log(response.data)
-                    setWeather(response.data)
-                    setIsLoading(false)
-                }
-            })
-    }, [])
+  if (!isLoading) {
+    // aqui tienes que comprobar que weather_icons no sea undefined
 
-    if (!isLoading) {
+    const icon =
+      (weather && weather.current && weather.current.weather_icons) || '';
+    const description =
+      (weather &&
+        weather.current &&
+        weather &&
+        weather.current.weather_descriptions) ||
+      '';
+    const windSpeed =
+      (weather && weather.current && weather && weather.current.wind_speed) ||
+      '';
+    const windDirection =
+      (weather && weather.current && weather && weather.current.wind_dir) || '';
 
-        const icon = weather.current.weather_icons;
-        const description = weather.current.weather_descriptions;
-        const windSpeed = weather.current.wind_speed;
-        const windDirection = weather.current.wind_dir;
+    return (
+      <div>
+        <h2>Weather in {capital}</h2>
+        <p>
+          <strong>Temperature: </strong>
+          {weather &&
+            weather.current &&
+            weather &&
+            weather.current.temperature}{' '}
+          celsius
+        </p>
+        <p>
+          <strong>Conditions:</strong> {description}
+        </p>
+        <img src={icon} alt='description icon'></img>
+        <p>
+          <strong>Wind: </strong>
+          {windSpeed} mph <strong>Direction: </strong>
+          {windDirection}
+        </p>
+      </div>
+    );
+  } else {
+    return <div>Loading Weather...</div>;
+  }
+};
 
-        return (
-            <div>
-                <h2>Weather in {capital}</h2>
-                <p><strong>Temperature: </strong>{weather.current.temperature} celsius</p>
-                <p><strong>Conditions:</strong> {description}</p>
-                <img src={icon} alt="description icon"></img>
-                <p><strong>Wind: </strong>{windSpeed} mph <strong>Direction: </strong>{windDirection}</p>
-            </div>
-        )
-    } else {
-        return (<div>Loading Weather...</div>)
-    }
-
-}
-
-export default CountrieFull
+export default CountrieFull;
